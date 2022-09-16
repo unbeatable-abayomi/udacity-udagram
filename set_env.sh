82262b15cf1f511b7fab977c4c1ddd22d6785251
abayomi <mac@macs-MacBook-Pro.local>
2022-09-16 10:44:38 +0100

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udagramdevpic
+export POSTGRES_PASSWORD=udagramdevpic
+export POSTGRES_HOST=udagramdevpic.cxlnxakyrpfx.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagramdevpic
+export AWS_BUCKET=arn:aws:s3:::udgramdevpics
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
